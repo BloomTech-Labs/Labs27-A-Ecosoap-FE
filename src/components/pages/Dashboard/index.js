68c16173337a166f9d524de859310ac39b8f6d11
hera <a.heraimenka@gmail.com>
2020-10-05 14:36:07 -0700

    feat: add header links

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { ordersGet } from '../../../state/actions/ordersActions';
 import Order from '../../common/Order';
+import Header from '../../common/Header';
 
 function Dashboard(props) {
   // load orders on first render
-  useEffect(() => {
-    props.ordersGet();
-  }, []);
+  useEffect(props.ordersGet, []);
 
   return (
     <>
+      <Header />
       <h1>Dashboard</h1>
       <div>
         {props.orders &&
