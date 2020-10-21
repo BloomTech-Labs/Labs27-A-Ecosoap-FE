1873a8159fd7cd0d6dddb7513f5743d660be9a68
William Sedlacek <wsedlacekc@gmail.com>
2020-10-21 11:08:26 -0700

    fix: editting reducer
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/state/reducers/profileReducer.js
+++ src/state/reducers/profileReducer.js
@@ -2,6 +2,9 @@ import {
   PROFILE_GET_LOADING,
   PROFILE_GET_FAILURE,
   PROFILE_GET_SUCCESS,
+  PROFILE_EDIT_LOADING,
+  PROFILE_EDIT_SUCCESS,
+  PROFILE_EDIT_FAILURE,
 } from '../actions/actionTypes';
 
 const initialState = {
@@ -12,12 +15,15 @@ const initialState = {
 
 export default function profileReducer(state = initialState, action) {
   switch (action.type) {
+    case PROFILE_EDIT_LOADING:
     case PROFILE_GET_LOADING:
       return { ...state, loading: true };
 
+    case PROFILE_EDIT_SUCCESS:
     case PROFILE_GET_SUCCESS:
       return { ...state, loading: false, profile: action.payload };
 
+    case PROFILE_EDIT_FAILURE:
     case PROFILE_GET_FAILURE:
       return { ...state, loading: false, error: action.payload };
     default:
